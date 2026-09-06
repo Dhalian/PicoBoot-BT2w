7c6d861170b432cd9837286a4d5391321de89bf0
Dhalian <67284307+Dhalian@users.noreply.github.com>
2026-09-06 21:53:14 +0200

    Add web_ui.h with HTML content for UI

---
+++ src/web_ui.h
@@ -0,0 +1,19 @@
+#ifndef WEB_UI_H
+#define WEB_UI_H
+
+static const char WEB_UI_INDEX_HTML[] =
+    "<!DOCTYPE html>\r\n"
+    "<html lang=\"fr\">\r\n"
+    "<head>\r\n"
+    "<meta charset=\"utf-8\">\r\n"
+    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\r\n"
+    "<title>PicoBoot BT - Configuration manettes</title>\r\n"
+    "<link rel=\"stylesheet\" href=\"https://homebrewfrance.fr/picoboot/style.css\">\r\n"
+    "</head>\r\n"
+    "<body>\r\n"
+    "<div id=\"app\">Chargement...</div>\r\n"
+    "<script src=\"https://homebrewfrance.fr/picoboot/app.js\"></script>\r\n"
+    "</body>\r\n"
+    "</html>\r\n";
+
+#endif // WEB_UI_H
